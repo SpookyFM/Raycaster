dfc660f6c90f31ca8e97141945c18ecc336ba656
Florian Mehm <florian@mehm.net>
2017-10-20 13:40:16 +0200

    Ray casting algorithm finally working correctly.

--- Sources/Exercise.cpp
+++ Sources/Exercise.cpp
@@ -19,7 +19,8 @@ using namespace Kore;
 static constexpr unsigned int LevelWidth = 10;
 static constexpr unsigned int LevelHeight = 10;
 
-float CellSize = 100.0f;
+const float CellSize = 100.0f;
+const float TextureSize = 64.0f;
 
 Kore::vec3 Level[LevelWidth][LevelHeight];
 
@@ -139,6 +140,20 @@ namespace {
 		return Color.x() > 0.0f || Color.y() > 0.0f || Color.z() > 0.0f;
 	}
 
+	int Sign(float Value)
+	{
+		float TestValue = Value + 0.0000001f;
+		return Value >= 0.0f ? 1 : -1;
+	}
+
+	void AssertSign(float Value, int SignValue)
+	{
+		if (Kore::abs(Value) > 0.00000001f)
+		{
+			assert(Sign(Value) == SignValue);
+		}
+	}
+
 	/** Casts a ray and returns the color of the wall at this position */
 	// Coordinate system has the point (0, 0) at the top-left corner of the map
 	// Increasing x to the right
@@ -147,51 +162,53 @@ namespace {
 	float CastRay(Kore::vec2 Position, float Angle, Kore::vec3& Result, Kore::vec2i& HitCell, int& TexCoordX, bool Debug = false)
 	{
 		if (Debug) Kore::log(Info, "Position %.2f%.2f, Angle %.2f", Position.x(), Position.y(), RadToDegrees(Angle));
-		// Check for edge cases
+		// Check for edge cases at multiples of 90 degrees
 		float fmodResult = fmod(Angle, (Kore::pi * 0.5f));
 		float d = Kore::abs(fmodResult / (Kore::pi * 0.5f));
 
-		float WrappedAngle = Angle;
-		float SinAngle = Kore::sin(WrappedAngle);
-		float CosAngle = Kore::cos(WrappedAngle);
+		float SinAngle = Kore::sin(Angle);
+		float CosAngle = Kore::cos(Angle);
 		float TanAngle = SinAngle / CosAngle;
 		const float epsilon = 0.01f;
 		if (d < epsilon || Kore::abs(1.0f - d) < epsilon)
 		{
-			// Worry about those later
+			// Worry about edge cases later
 			return CellSize * (LevelHeight * LevelWidth);
 		}
-
-
-		Kore::vec2i CurrentCell = GetCell(Position);
-		// We want to check the intersections with the grid cells
-		const Kore::vec2 PositionInCurrentCell = GetPositionInCurrentCell(Position);
-		// First, advance horizontally
-
 		// In which direction are we pointing?
-		int SignHorizontal = Kore::cos(WrappedAngle) > 0.0f ? 1 : -1;
-		int SignVertical = Kore::sin(WrappedAngle) > 0.0f ? -1 : 1;
-
-
+		int SignHorizontal = CosAngle > 0.0f ? 1 : -1;
+		int SignVertical = SinAngle > 0.0f ? -1 : 1;
 		if (Debug) Kore::log(Info, "Tan: %f, SignHorizontal %i, SignVertical %i", TanAngle, SignHorizontal, SignVertical);
 
+		// These values will indicate if we had a horizontal or vertical hit or both (in this case, the shorter distance is taken)
+		Kore::vec3 HorizontalColor;
+		float HorizontalDistance = -1.0f;
+		Kore::vec2i HitHorizontalCell;
+
+		Kore::vec3 VerticalColor;
+		float VerticalDistance = -1.0f;
+		Kore::vec2i HitVerticalCell;
+
+		Kore::vec2i CurrentCell = GetCell(Position);
+		const Kore::vec2 PositionInCurrentCell = GetPositionInCurrentCell(Position);
+
+		//////////////////////////////////////////////////////////////////////////
+		// First, advance horizontally and check for the first intersection with a cell border on the y-axis
+		//////////////////////////////////////////////////////////////////////////
 		// Compute the delta to the first intersection
 		float FirstDeltaX = SignHorizontal > 0.0f ? CellSize - PositionInCurrentCell.x() : -PositionInCurrentCell.x();
 		// On the y-axis, we need to multiply with -1 in order to account for the coordinate system being the other way around than the unit circle.
 		float FirstDeltaY = -TanAngle * FirstDeltaX;
+		AssertSign(FirstDeltaX, SignHorizontal);
+		AssertSign(FirstDeltaY, SignVertical);
 
 		// This is the first intersection horizontally
-		Kore::vec3 HorizontalColor;
-		float HorizontalDistance = -1.0f;
-
+		// We find the x-value by just stepping one cell
 		if (Debug) Kore::log(Info, "Testing horizontal: %0.2f|%0.2f", CurrentPosition.x() + FirstDeltaX, CurrentPosition.y() + FirstDeltaY);
 		Kore::vec2i TestCell = Kore::vec2i(
-			//CurrentCell.x() + SignHorizontal,
-			(int)Kore::floor((Position.x() + FirstDeltaX) / CellSize),
+			CurrentCell.x() + SignHorizontal,
 			(int)Kore::floor((Position.y() + FirstDeltaY) / CellSize)
 		);
-
-		Kore::vec2i HitHorizontalCell;
 
 		Kore::vec3 CurrentColorHorizontal = GetColor(TestCell);
 		if (IsSolid(CurrentColorHorizontal))
@@ -199,54 +216,58 @@ namespace {
 			HorizontalColor = CurrentColorHorizontal;
 			HorizontalDistance = FirstDeltaX;
 			HitHorizontalCell = TestCell;
-			if (Debug) Kore::log(Info, "Horizontal: Hit test on first attempt.");
+			if (Debug) Kore::log(Info, "Horizontal: Hit result in initial test.");
 		}
 		else
 		{
 			// We need to iterate from this point
-			unsigned int NumHorizontalChecks = SignHorizontal > 0 ? LevelWidth - CurrentCell.x() : CurrentCell.x();
 			// Inverted due to inverted y-axis
-			float DeltaY = -TanAngle * CellSize * SignVertical;
+			float DeltaY = -TanAngle * CellSize * SignHorizontal;
+			AssertSign(DeltaY, SignVertical);
 			float CurrentY = Position.y() + FirstDeltaY;
-			// If we walk to the left, we are checking for the right side of the cell
-			//TestCell[0] += SignHorizontal < 0 ? -1 : 0;
-			for (unsigned int i = 0; i < NumHorizontalChecks; i++)
+			//@@TODO: Figure out what the upper limit of tests is actually - now, we are relying on the level to be bounded
+			// At this point, we are at the first intersection at the border of our current cell
+			// i counts the number of cells we have moved away from the first tested cell
+			for (int i = 1;;i++)
 			{
+				// In x-direction, we are advancing one cell at a time
 				TestCell[0] += SignHorizontal;
+				// In y-direction, we are advancing the associated DeltaY
 				CurrentY += DeltaY;
 				TestCell[1] = (int)Kore::floor(CurrentY / CellSize);
-				if (Debug) Kore::log(Info, "Testing horizontal: %0.2f|%0.2f", TestCell.x() * CellSize, CurrentY);
+				if (Debug) Kore::log(Info, "Testing horizontal cell: %i|%i", TestCell.x(), TestCell.y());
 				if (TestCell.y() < 0 || TestCell.y() >= LevelHeight)
 				{
 					if (Debug) Kore::log(Info, "Horizontal: Out of bounds.");
 					break;
 				}
+
 				CurrentColorHorizontal = GetColor(TestCell);
 				if (IsSolid(CurrentColorHorizontal))
 				{
-					HorizontalDistance = FirstDeltaX + CellSize * (i + 1) * SignHorizontal;
-					// HorizontalDistance += SignHorizontal < 0 ? CellSize: 0.0f;
+					// We have hit a cell from the left or the right
+					// Horizontally, we have moved the initial distance to the first cell border plus i additional cells
+					HorizontalDistance = FirstDeltaX + CellSize * i * SignHorizontal;
 					HorizontalColor = CurrentColorHorizontal;
 					HitHorizontalCell = TestCell;
-					Kore::vec2 HitPoint(TestCell[0] * CellSize, CurrentY);
-					float TestHorizontalDistance = HitPoint[0] - CurrentPosition[0];
-					// assert(Kore::abs(TestHorizontalDistance - HorizontalDistance) < 0.5f);
-					if (Debug) Kore::log(Info, "Horizontal: Hit at point: %.2f|%.2f, distance %.2f", HitPoint.x(), HitPoint.y(), (HitPoint - CurrentPosition).getLength());
+					if (Debug) Kore::log(Info, "Horizontal: Hit at cell %i|%i", HitHorizontalCell.x(), HitHorizontalCell.y());
 					break;
 				}
 			}
 		}
 
-		Kore::vec3 VerticalColor;
-		float VerticalDistance = -1.0f;
-		Kore::vec2i HitVerticalCell;
-		
+		//////////////////////////////////////////////////////////////////////////
+		// Second, advance vertically and check for the first intersection with a cell border on the x-axis
+		//////////////////////////////////////////////////////////////////////////
+		// Compute the delta to the first intersection
+		// On the y-axis, we need to multiply with -1 in order to account for the coordinate system being the other way around than the unit circle.
 		FirstDeltaY = SignVertical > 0.0f ? CellSize - PositionInCurrentCell.y() : -PositionInCurrentCell.y();
 		FirstDeltaX = -FirstDeltaY / TanAngle;
+		AssertSign(FirstDeltaX, SignHorizontal);
+		AssertSign(FirstDeltaY, SignVertical);
 
-		if (Debug) Kore::log(Info, "Testing vertical: %0.2f|%0.2f", CurrentPosition.x() + FirstDeltaX, CurrentPosition.y() + FirstDeltaY);
-
-		// Now, the same for vertical
+		// This is the first intersection vertically 
+		if (Debug) Kore::log(Info, "Testing vertically: %0.2f|%0.2f", CurrentPosition.x() + FirstDeltaX, CurrentPosition.y() + FirstDeltaY);
 		TestCell = Kore::vec2i(
 			(int)Kore::floor((Position.x() + FirstDeltaX) / CellSize),
 			CurrentCell.y() + SignVertical
@@ -258,68 +279,78 @@ namespace {
 			VerticalColor = CurrentColorVertical;
 			VerticalDistance = FirstDeltaY;
 			HitVerticalCell = TestCell;
-			if (Debug) Kore::log(Info, "Vertical: Hit test on first attempt.");
+			if (Debug) Kore::log(Info, "Vertical: Hit result in initial test.");
 		}
 		else
 		{
 			// We need to iterate from this point
-			// If we are moving up (SignVertical = -1) we are hitting the lower bounds of the cell, if we are moving down (SignVertical = 1) we are hitting the top of the cell
-			unsigned int NumVerticalChecks = SignVertical > 0 ? LevelHeight - CurrentCell.y() : CurrentCell.y();
-			float DeltaX = -CellSize / TanAngle * SignHorizontal;
+			// Inverted due to inverted y-axis
+			float DeltaX = -CellSize / TanAngle * SignVertical;
+			AssertSign(DeltaX, SignHorizontal);
 			float CurrentX = Position.x() + FirstDeltaX;
-			// Adjust for the direction
-			// TestCell[1] += SignVertical > 0 ? 0 : 1;
-			for (unsigned int i = 0; i < NumVerticalChecks; i++)
+			//@@TODO: Figure out what the upper limit of tests is actually - now, we are relying on the level to be bounded
+			// At this point, we are at the first intersection at the border of our current cell
+			// i counts the number of cells we have moved away from the first tested cell
+			for (int i = 1;;i++)
 			{
+				// In y-direction, we are advancing one cell at a time
 				TestCell[1] += SignVertical;
+				// In x-direction, we are advancing the associated DeltaY
 				CurrentX += DeltaX;
 				TestCell[0] = (int)Kore::floor(CurrentX / CellSize);
-				if (Debug) Kore::log(Info, "Testing vertical: %0.2f|%0.2f", CurrentX, TestCell[1] * CellSize);
-				if (TestCell.x() < 0 || TestCell.x() >= LevelWidth)
+				if (Debug) Kore::log(Info, "Testing vertical cell: %i|%i", TestCell.x(), TestCell.y());
+				if (TestCell.y() < 0 || TestCell.y() >= LevelHeight)
 				{
-					if (Debug) Kore::log(Info, "Vertical: Out of bounds.");
+					if (Debug) Kore::log(Info, "Horizontal: Out of bounds.");
 					break;
 				}
+
 				CurrentColorVertical = GetColor(TestCell);
 				if (IsSolid(CurrentColorVertical))
 				{
-					//VerticalDistance = FirstDeltaY + CellSize * (i + 1) * SignVertical;
-					VerticalDistance = FirstDeltaY + CellSize * (i + 1) * SignVertical;
-					VerticalDistance += SignVertical > 0 ? 0.0f : -100.0f;
+					// We have hit a cell from the top or the bottom 
+					// Vertically, we have moved the initial distance to the first cell border plus i additional cells
+					VerticalDistance = FirstDeltaY + CellSize * i * SignVertical;
 					VerticalColor = CurrentColorVertical;
 					HitVerticalCell = TestCell;
-					Kore::vec2 HitPoint(CurrentX, TestCell[1] * CellSize);
-					if (Debug) Kore::log(Info, "Vertical: Hit at point: %.2f|%.2f, distance %.2f", HitPoint.x(), HitPoint.y(), (HitPoint - CurrentPosition).getLength());
+					if (Debug) Kore::log(Info, "Vertical: Hit at cell %i|%i", HitVerticalCell.x(), HitVerticalCell.y());
 					break;
 				}
 			}
-		}
+		} 
 
-		float Distance = CellSize * Kore::max(LevelWidth, LevelHeight);
+		//////////////////////////////////////////////////////////////////////////
+		// Now, check which distance is shorter
+		//////////////////////////////////////////////////////////////////////////
+		float Distance = CellSize * Kore::max(LevelWidth, LevelHeight) + 100000.0f;
 		if (IsSolid(HorizontalColor))
 		{
+			// For the vertical distance, we need to invert again
 			float Y = -TanAngle * HorizontalDistance;
-			// Distance = Kore::sqrt(HorizontalDistance * HorizontalDistance + Y * Y);
+			AssertSign(HorizontalDistance, SignHorizontal);
+			AssertSign(Y, SignVertical);
 			Distance = Kore::cos(CurrentAngle) * HorizontalDistance - Kore::sin(CurrentAngle) * Y;
 			Result = HorizontalColor;
 			HitCell = HitHorizontalCell;
-			float yHitPosition = fmod(CurrentPosition.y() + Y, 100.0f) / 100.0f;
-			TexCoordX = (int)(yHitPosition * 64.0f);
-		}
+			float yHitPosition = fmod(CurrentPosition.y() + Y, CellSize) / CellSize;
+			TexCoordX = (int)(yHitPosition * TextureSize);
+		} 
 		if (IsSolid(VerticalColor))
 		{
+			// For the vertical distance, we need to invert again
 			float X = -VerticalDistance / TanAngle;
-			//float TestDistance = Kore::sqrt(VerticalDistance * VerticalDistance + X * X);
-			float TestDistance = Kore::cos(CurrentAngle) * X - Kore::sin(CurrentAngle) * VerticalDistance;
-			if (TestDistance < Distance)
+			AssertSign(X, SignHorizontal);
+			AssertSign(VerticalDistance, SignVertical);
+			float TempDistance = Kore::cos(CurrentAngle) * X - Kore::sin(CurrentAngle) * VerticalDistance;
+			if (TempDistance < Distance)
 			{
-				Distance = TestDistance;
+				Distance = TempDistance;
 				Result = VerticalColor;
 				HitCell = HitVerticalCell;
-				float xHitPosition = fmod(CurrentPosition.x() + X, 100.0f) / 100.0f;
-				TexCoordX = (int)(xHitPosition * 64.0f);
+				float xHitPosition = fmod(CurrentPosition.x() + X, CellSize) / CellSize;
+				TexCoordX = (int)(xHitPosition * TextureSize);
 			}
-		}
+		} 
 		return Distance;
 	}
 
@@ -394,6 +425,8 @@ namespace {
 		// CurrentAngle = -0.2f;
 		float TestDistance = CastRay(CurrentPosition, CurrentAngle, CurrentColor, HitCell, TexCoordX, true);
 		Kore::vec2i Cell = GetCell(CurrentPosition);
+		bool IsInsideBlock = IsSolid(GetColor(Cell));
+		assert(!IsInsideBlock);
 		// Kore::log(Info, "DeltaT: %f", DeltaT);
 		Kore::log(Info, "Player is at %.1f|%.1f, angle %.3f, cell %i|%i, hit cell %i|%i distance is %.2f", CurrentPosition.x(), CurrentPosition.y(), RadToDegrees(CurrentAngle), Cell.x(), Cell.y(), HitCell.x(), HitCell.y(), TestDistance);
 		// Kore::log(Info, "Tex Coord X: %f", TexCoordX);
